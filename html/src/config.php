fd4ffcc473ac55df31e3601b0acf1639b0de0de4
AntzCode <biz@antzcode.com>
2024-03-18 18:46:20 +1300

    feat: Add screenshot and increment version for release of vouchers feature

--- html/src/config.php
+++ html/src/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $fuppiConfig = [
-    'fuppi_version' => '1.0.3r1',
+    'fuppi_version' => '1.0.3',
     'sqlite3FilePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'FUPPI_DB.sqlite3',
     'uploadedFilesPath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'uploadedFiles',
     'post_max_size' => ini_get('post_max_size')
